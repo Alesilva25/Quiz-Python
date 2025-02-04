d09d71778c722200d0e8978c22fc2538be21bc71
Alexandro Silva <Alexandro18silvas@gmail.com>
2025-02-04 15:38:12 -0300

    Coloquei umas cores

--- quiz.py
+++ quiz.py
@@ -1,3 +1,7 @@
+from colorama import init, Fore
+
+init()
+
 print("Seja muito bem vindo ao Quiz do Ale!")
 answer_user = input("Quer começar? (S/N) ").upper()
 print(answer_user)
@@ -6,18 +10,18 @@ if answer_user != "S":
     quit()
 
 print("Começando...\n")
-print("Quem desenvolveu o jogo Grand Theft Auto (GTA)? \n (A) Rockstar Games \n (B) Ubisoft \n (C) Activision \n (D) EA \n")
+print(Fore.YELLOW + "Quem desenvolveu o jogo Grand Theft Auto (GTA)? \n" + Fore.WHITE + " (A) Rockstar Games \n (B) Ubisoft \n (C) Activision \n (D) EA \n")
 answer1 = input("Resposta: ").upper()
 
 if answer1 == "A":
-    print("Correto!\n")
+    print(Fore.GREEN + "Correto!\n" + Fore.WHITE)
 else: 
-    print("Incorreto\n")
+    print(Fore.RED + "Incorreto\n" + Fore.WHITE)
 
-print("Qual o nome do protagonista do jogo GTA San Andreas?\n (A) Carlos John\n (B) Carl Johnson\n (C) Carl Jaqueline\n (D) Carlos Jonhson")
+print(Fore.YELLOW + "Qual o nome do protagonista do jogo GTA San Andreas?\n" + Fore.WHITE + " (A) Carlos John\n (B) Carl Johnson\n (C) Carl Jaqueline\n (D) Carlos Jonhson")
 answer2 = input("Resposta: ").upper()
 
 if answer2 == "B":
-    print("Correto!\n")
-else:
-    print("Incorreto\n")
+    print(Fore.GREEN + "Correto!\n" + Fore.WHITE)
+else: 
+    print(Fore.RED + "Incorreto\n" + Fore.WHITE)
